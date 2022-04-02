690b4131e6614eeb4d643daf37f823f429ab96ee
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-03 00:59:10 +0600

    Header section done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import Coins from './components/Coins/Coins';
+import Content from './components/Content/Content';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+        <Routes>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/coins' element={<Coins />}></Route>
+          <Route path='/content' element={<Content />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className="flex justify-center items-center  bg-slate-200 md:justify-between px-4 h-[100px]">
+            <div>
+                <h1 className="font-mono text-3xl hidden md:block font-bold hover:text-orange-300 ">₿Crypto Geeks</h1>
+            </div>
+            <div className="flex gap-4 text-xl font-sans font-bold text-blue-500 hover:text-blue-700">
+                <NavLink to='/home' className={({isActive}) => isActive ? "text-orange-400 underline underline-offset-4" : ""}>
+                    Home
+                </NavLink>
+
+                <NavLink to='/coins' className={({isActive}) => isActive ? "text-orange-400 underline underline-offset-4" : ""}>
+                    Coins
+                </NavLink>
+
+                <NavLink to='/content' className={({isActive}) => isActive ? "text-orange-400 underline underline-offset-4" : ""}>
+                    Content
+                </NavLink>
+                
+                <NavLink to='/about'>
+                    About
+                </NavLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
