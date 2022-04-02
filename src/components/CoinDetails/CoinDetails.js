8083327ef540649a95d3516eb0eed494dea2b449
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-03 02:43:36 +0600

    coin details page added

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -7,9 +7,9 @@ const CoinDetails = () => {
     const { id } = useParams();
     const [details, setDetails] = useState({});
     console.log(details);
-    const { name, symbol, market_cap_rank, country_origin,contact, hashing_algorithm, genesis_date, last_updated, community_score, developer_score, liquidity_score, public_interest_score  } = details;
+    const { name, symbol, market_cap_rank, country_origin,contact, hashing_algorithm, genesis_date, last_updated, community_score, developer_score, liquidity_score, public_interest_score} = details;
 
-    const {image} = details?.image?.large;
+    // const {image} = details?.image?.large;
 
     useEffect( () => {
         const url = ` https://api.coingecko.com/api/v3/coins/${id}`
@@ -19,9 +19,9 @@ const CoinDetails = () => {
     }, [id]);
     
     return (
-        <div className="flex flex-col  justify-between mx-auto w-[800px] text-left items-center md:flex-row">
-            <div className="order-2">
-                <h1>General Info</h1>
+        <div className="flex flex-col justify-between md:w-[800px]  text-left items-center mt-16 md:flex-row mx-auto">
+            <div className="order-2 md:order-1">
+                <h1 className="text-4xl mt-4">General Info</h1>
                 <hr />
                <p>Coin name: {name}</p>
                <p>Market cap rank: {market_cap_rank}</p>
@@ -30,15 +30,15 @@ const CoinDetails = () => {
                <p>Hasing Algorithm: {hashing_algorithm}</p>
                <p>Genesis Date: {genesis_date}</p>
                <p>Last Update: {last_updated}</p>
-               <h1>Scroes: </h1>
+               <h1 className="text-4xl mt-4">Scroes: </h1>
                <hr />
                <p>Commiunity Score: {community_score}</p>
                <p>Developer Score: {developer_score}</p>
                <p>Liqidity Score: {liquidity_score}</p>
                <p>Piblic Inteset Score: {public_interest_score}</p>
             </div>
-            <div className="order-1">
-                <img src={image} alt="" />
+            <div className="order-1 md:order-2">
+                <img src={details.image?.large} alt="" />
             </div>
         </div>
     );
