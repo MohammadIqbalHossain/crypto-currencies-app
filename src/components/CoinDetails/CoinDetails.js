6b4a2703138073825ffd4e32fc15ed74c3061566
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-03 02:23:31 +0600

    Details info added

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -7,6 +7,9 @@ const CoinDetails = () => {
     const { id } = useParams();
     const [details, setDetails] = useState({});
     console.log(details);
+    const { name, symbol, market_cap_rank, country_origin,contact, hashing_algorithm, genesis_date, last_updated, community_score, developer_score, liquidity_score, public_interest_score  } = details;
+
+    const {image} = details?.image?.large;
 
     useEffect( () => {
         const url = ` https://api.coingecko.com/api/v3/coins/${id}`
@@ -16,12 +19,26 @@ const CoinDetails = () => {
     }, [id]);
     
     return (
-        <div>
-            <div>
-             
+        <div className="flex flex-col  justify-between mx-auto w-[800px] text-left items-center md:flex-row">
+            <div className="order-2">
+                <h1>General Info</h1>
+                <hr />
+               <p>Coin name: {name}</p>
+               <p>Market cap rank: {market_cap_rank}</p>
+               <p>Origin: {country_origin ? country_origin : "Not avilable"}</p>
+               <p>Contact Address: {contact}</p>
+               <p>Hasing Algorithm: {hashing_algorithm}</p>
+               <p>Genesis Date: {genesis_date}</p>
+               <p>Last Update: {last_updated}</p>
+               <h1>Scroes: </h1>
+               <hr />
+               <p>Commiunity Score: {community_score}</p>
+               <p>Developer Score: {developer_score}</p>
+               <p>Liqidity Score: {liquidity_score}</p>
+               <p>Piblic Inteset Score: {public_interest_score}</p>
             </div>
-            <div>
-
+            <div className="order-1">
+                <img src={image} alt="" />
             </div>
         </div>
     );
